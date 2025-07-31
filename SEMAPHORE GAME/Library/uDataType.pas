61489e7688f5f5306459204f2c385d7fdcf8c270
lo7thdrag <princeachmad@gmail.com>
2025-07-31 14:35:34 +0700

    update

--- SEMAPHORE GAME/Library/uDataType.pas
+++ SEMAPHORE GAME/Library/uDataType.pas
@@ -12,6 +12,18 @@ unit uDataType;
 interface
 
 const
+  C_ABJAD_ID : array [0..27] of string = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                                           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'numeral', 'spasi');
+  C_ABJAD_RND1 : array [0..27] of string = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                                           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'numeral', 'spasi');
+  C_ABJAD_RND2 : array [0..27] of string = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                                           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'numeral', 'spasi');
+  C_ABJAD_RND3 : array [0..27] of string = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                                           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'numeral', 'spasi');
+  C_ABJAD_RND4 : array [0..27] of string = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                                           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'numeral', 'spasi');
+  C_ABJAD_RND5 : array [0..27] of string = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                                           'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'numeral', 'spasi');
   {$region ' Soal-Soal mode Easy '}
   C_EASY1 : array [0..9] of string = ('Bunga', 'Layar', 'Rambu', 'Tenda', 'Laris', 'Surat', 'Koran', 'Minum', 'Lemak', 'Sinar');
   C_EASY2 : array [0..9] of string = ('Lapar', 'Rumah', 'Pisau', 'Pulau', 'Belut', 'Badan', 'Senin', 'Tanah', 'Datar', 'Pedas');
@@ -35,7 +47,7 @@ const
 
   C_NORMAL6 : array [0..9] of string = ('jalan kaki', 'pukul tiga', 'sayang dia', 'cinta anak', 'gagal mulu', 'butuh kamu', 'jarak jauh', 'kawat besi', 'suka gulat', 'bola tenis');
   C_NORMAL7 : array [0..9] of string = ('lihat mata', 'tarik ulur', 'hutan riau', 'nyeri kaki', 'jemur baju', 'jamur enak', 'tarik tali', 'saya tobat', 'sadar diri', 'kamu kalah');
-  C_NORMAL  : array [0..9] of string = ('lihat saya', 'tidur pagi', 'jeruk cina', 'sawi hijau', 'kuda putih', 'Beras enak', 'pulau rote', 'mandi lama', 'gagal lagi', 'tidak akan');
+  C_NORMAL8  : array [0..9] of string = ('lihat saya', 'tidur pagi', 'jeruk cina', 'sawi hijau', 'kuda putih', 'Beras enak', 'pulau rote', 'mandi lama', 'gagal lagi', 'tidak akan');
   C_NORMAL9 : array [0..9] of string = ('bakar ikan', 'kaca retak', 'takut lama', 'lima pisau', 'bawah meja', 'sakit gigi', 'rindu ayah', 'sorot mata', 'bola tenis', 'pikir dulu');
   C_NORMAL10 : array [0..9] of string = ('lebih lama', 'cium bunda', 'kawat besi', 'sikat gigi', 'rindu ayah', 'pasar daya', 'jaket baru', 'keras batu', 'mata indah', 'sayang ibu');
   {$ENDREGION}
@@ -49,42 +61,62 @@ const
 
   C_HARD6 : array [0..9] of string = ('surabaya makmur', 'gagal coba lagi', 'bayam dan tomat', 'roti merah muda', 'nyeri kaki saya', 'jalan panjangku', 'cuaca yang baik', 'rapat guru guru', 'tanah ayah saya', 'bantu saya dulu');
   C_HARD7 : array [0..9] of string = ('tni bela negara', 'hidup mati saya', 'strategi perang', 'nyeri kaki saya', 'saya jemur baju', 'surabaya makmur', 'tarik ulur tali', 'badan saya kaku', 'kemenangan kami', 'kalah lagi saya');
-  C_HARDL : array [0..9] of string = ('pos jaga markas', 'jika kamu lihat', 'petai cina saya', 'tempatnya indah', 'jajan kaki lima', 'rapat guru guru', 'lomba lari lari', 'mandi pagi hari', 'makan ikan laut', 'kami cinta mama');
+  C_HARD8 : array [0..9] of string = ('pos jaga markas', 'jika kamu lihat', 'petai cina saya', 'tempatnya indah', 'jajan kaki lima', 'rapat guru guru', 'lomba lari lari', 'mandi pagi hari', 'makan ikan laut', 'kami cinta mama');
   C_HARD9 : array [0..9] of string = ('strategi perang', 'rindu mama saya', 'takut akan ular', 'lekuk kaki saya', 'mimpi tingginya', 'hewan baik baik', 'lari lari malam', 'pilih baju baru', 'kerja pagi lagi', 'pilih lauk ayam');
   C_HARD10 : array [0..9] of string = ('kemenangan kami', 'lihat saya baik', 'lupa bawa kunci', 'gigi putih saya', 'panjang jalanku', 'cuaca yang baik', 'sikap baik kamu', 'hidup saya atur', 'masak teri nasi', 'saya cari cari');
   {$ENDREGION}
 
-  function GetQuestion(const exerciseMode, randomValue: Integer): string;
+  function GetQuestion(const exerciseMode, randomValue, nomorSoal: Integer): string;
 
 implementation
 
-function GetQuestion(const exerciseMode, randomValue: Integer): string;
+function GetQuestion(const exerciseMode, randomValue, nomorSoal: Integer): string;
 begin
   case exerciseMode of
     0: {Easy}
     begin
       case randomValue of
-        0: {Easy}
-        begin
-
-        end;
-        1: {Normal}
-        begin
-
-        end;
-        2: {Hard}
-        begin
-
-        end;
+        1: Result := C_EASY1[nomorSoal];
+        2: Result := C_EASY2[nomorSoal];
+        3: Result := C_EASY3[nomorSoal];
+        4: Result := C_EASY4[nomorSoal];
+        5: Result := C_EASY5[nomorSoal];
+        6: Result := C_EASY6[nomorSoal];
+        7: Result := C_EASY7[nomorSoal];
+        8: Result := C_EASY8[nomorSoal];
+        9: Result := C_EASY9[nomorSoal];
+        10: Result := C_EASY10[nomorSoal];
       end;
     end;
     1: {Normal}
     begin
-
+      case randomValue of
+        1: Result := C_NORMAL1[nomorSoal];
+        2: Result := C_NORMAL2[nomorSoal];
+        3: Result := C_NORMAL3[nomorSoal];
+        4: Result := C_NORMAL4[nomorSoal];
+        5: Result := C_NORMAL5[nomorSoal];
+        6: Result := C_NORMAL6[nomorSoal];
+        7: Result := C_NORMAL7[nomorSoal];
+        8: Result := C_NORMAL8[nomorSoal];
+        9: Result := C_NORMAL9[nomorSoal];
+        10: Result := C_NORMAL10[nomorSoal];
+      end;
     end;
     2: {Hard}
     begin
-
+      case randomValue of
+        1: Result := C_HARD1[nomorSoal];
+        2: Result := C_HARD2[nomorSoal];
+        3: Result := C_HARD3[nomorSoal];
+        4: Result := C_HARD4[nomorSoal];
+        5: Result := C_HARD5[nomorSoal];
+        6: Result := C_HARD6[nomorSoal];
+        7: Result := C_HARD7[nomorSoal];
+        8: Result := C_HARD8[nomorSoal];
+        9: Result := C_HARD9[nomorSoal];
+        10: Result := C_HARD10[nomorSoal];
+      end;
     end;
   end;
 end;

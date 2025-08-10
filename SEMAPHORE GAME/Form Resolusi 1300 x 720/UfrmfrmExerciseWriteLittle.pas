a71557691d4935ebd73b535a6a822042b1d1182b
Nfb12345 <nestafalsabryan@gmail.com>
2025-08-10 23:14:05 +0700

    Update  form ExerciseWriteLittle
    form Exercise Write Little

---
+++ SEMAPHORE GAME/Form Resolusi 1300 x 720/UfrmfrmExerciseWriteLittle.pas
@@ -0,0 +1,114 @@
+unit UfrmfrmExerciseWriteLittle;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls,
+  Vcl.StdCtrls;
+
+type
+  TfrmExerciseWriteLittle = class(TForm)
+    imgBackground: TImage;
+    pnlBackground: TPanel;
+    img18: TImage;
+    img19: TImage;
+    lblAgain: TLabel;
+    lblHome: TLabel;
+    lblNext: TLabel;
+    pnl2: TPanel;
+    pnlJawab1: TPanel;
+    pnlJawab11: TPanel;
+    Panel1: TPanel;
+    pnlJawab2: TPanel;
+    pnlJawab22: TPanel;
+    Panel4: TPanel;
+    pnlJawab3: TPanel;
+    pnlJawab33: TPanel;
+    Panel7: TPanel;
+    pnlJawab4: TPanel;
+    pnlJawab44: TPanel;
+    Panel10: TPanel;
+    pnlJawab5: TPanel;
+    pnlJawab55: TPanel;
+    Panel13: TPanel;
+    pnlJawab6: TPanel;
+    pnlJawab66: TPanel;
+    Panel16: TPanel;
+    pnlJawab7: TPanel;
+    pnlJawab77: TPanel;
+    Panel19: TPanel;
+    pnlJawab8: TPanel;
+    pnlJawab88: TPanel;
+    Panel22: TPanel;
+    pnlJawab9: TPanel;
+    pnlJawab99: TPanel;
+    Panel25: TPanel;
+    pnlJawab10: TPanel;
+    pnlJawab100: TPanel;
+    imgJawabHuruf3: TImage;
+    imgJawabanHuruf4: TImage;
+    imgJawabanHuruf5: TImage;
+    imgJawabanHuruf6: TImage;
+    imgJawabanHuruf7: TImage;
+    imgJawabanHuruf8: TImage;
+    imgJawabanHuruf9: TImage;
+    imgJawabanHuruf10: TImage;
+    imgJawabanHuruf11: TImage;
+    imgJawabanHuruf2: TImage;
+    imgJawabanHuruf12: TImage;
+    imgJawabanHuruf13: TImage;
+    imgJawabanHuruf14: TImage;
+    imgJawabanHuruf15: TImage;
+    imgA: TImage;
+    imgB: TImage;
+    imgC: TImage;
+    imgD: TImage;
+    imgE: TImage;
+    imgF: TImage;
+    imgG: TImage;
+    imgH: TImage;
+    imgI: TImage;
+    imgT: TImage;
+    imgU: TImage;
+    imgV: TImage;
+    imgW: TImage;
+    imgX: TImage;
+    imgY: TImage;
+    imgJ: TImage;
+    imgK: TImage;
+    imgL: TImage;
+    imgM: TImage;
+    imgN: TImage;
+    imgO: TImage;
+    imgP: TImage;
+    imgQ: TImage;
+    imgR: TImage;
+    imgS: TImage;
+    imgZ: TImage;
+    imgSpasi: TImage;
+    pnlUsername: TPanel;
+    img4: TImage;
+    lbl18: TLabel;
+    lblUsername: TLabel;
+    pnlExerciseMode: TPanel;
+    Image15: TImage;
+    blExerciseMode: TLabel;
+    lbl1: TLabel;
+    lblQuetions: TLabel;
+    pnlSoal: TPanel;
+    imgJawabanHuruf1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmExerciseWriteLittle: TfrmExerciseWriteLittle;
+
+implementation
+
+{$R *.dfm}
+
+end.
